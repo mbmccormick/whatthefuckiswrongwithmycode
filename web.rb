b396d2610aa170bcbfca8529885328140dc779e7
Matt McCormick <mbmccormick@gmail.com>
2012-03-20 10:41:07 -0300

    Update web.rb

--- web.rb
+++ web.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby -I ../lib -I lib
 require 'sinatra'
 
+data = ["try consulting an API", "use more for loops", "store the data in a hash table"]
+
 get '/' do
-  "Hello, world"
+  data.random
 end
 
