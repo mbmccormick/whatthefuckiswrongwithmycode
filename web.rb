c14c0bc01a4da237e0f84932860ab6dc1ceb21f6
Matt McCormick <mbmccormick@gmail.com>
2012-03-20 10:50:13 -0300

    Update web.rb

--- web.rb
+++ web.rb
@@ -4,6 +4,27 @@ require 'sinatra'
 data = ["try consulting an API", "use more for loops", "store the data in a hash table"]
 
 get '/' do
-  data.sample
+  erb :default, :locals => { :suggestion => data.sample }
 end
 
+__END__
+
+@@ layout
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>what the fuck is wrong with my code</title>
+    <style type="text/css">
+      body {
+        font-family: Helvetica;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>what the fuck is wrong with my code></h1>
+    <%= yield %>
+  </body>
+</html>
+
+@@ default
+<p><%= suggestion %></p>
